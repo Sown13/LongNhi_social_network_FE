7261515c88eb179486832bdc54b39f88713af82d
nguyennhuhung281197 <125869328+nguyennhuhung281197@users.noreply.github.com>
2023-07-21 09:00:38 +0700

    done register form - BE connected

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import MainPage from "./pages/MainPage";
 import {Route, Routes} from "react-router-dom";
-import NewFeed from "./pages/user/NewFeed";
+import NewFeed from "./pages/user/home/NewFeed";
+import FormRegister from "./components/FormRegister";
 
 function App() {
   return (
@@ -16,7 +17,9 @@ function App() {
           </Routes>
       </MainPage>
       <Footer></Footer>
+        <FormRegister></FormRegister>
     </div>
+
   );
 }
 

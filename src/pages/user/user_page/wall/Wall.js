5ac498c7900277d77826b92fd281f6668ca983cc
giangthidua <125744426+giangadua06042001@users.noreply.github.com>
2023-07-26 09:23:08 +0700

    temporary done - Wall - can post new post without image

--- src/pages/user/user_page/wall/Wall.js
+++ src/pages/user/user_page/wall/Wall.js
@@ -1,41 +1,90 @@
 import "./Wall.css";
 import {Link} from "react-router-dom";
 import {useState} from "react";
+import {Field, Form, Formik} from "formik";
+import axios from "axios";
 
 export default function Wall() {
+    const [imagePost, setImagePost] = useState(null);
 
-
-
-    const [user, setUser] = useState(
-        () => {
-            let loggedInUser = localStorage.getItem("user");
-            if (loggedInUser === null || loggedInUser === "undefined") {
-                loggedInUser = {
-                    message: "Login to access more features",
-                    userId: 0,
-                    accountName: "Guest",
-                    fullName: "Guest",
-                    role: "GUEST"
-                };
-            } else {
-                loggedInUser = JSON.parse(loggedInUser);
-            }
-            return loggedInUser;
+    const [user, setUser] = useState(() => {
+        let loggedInUser = localStorage.getItem("user");
+        if (loggedInUser === null || loggedInUser === "undefined") {
+            loggedInUser = {
+                message: "Login to access more features",
+                userId: 0,
+                accountName: "Guest",
+                fullName: "Guest",
+                role: "GUEST"
+            };
+        } else {
+            loggedInUser = JSON.parse(loggedInUser);
         }
-    )
-    return (
-        <div className={"newFeed"}>
-            <div className="newFeedContainer">
-                <br/>
+        return loggedInUser;
+    });
 
-                <div className={"feedCarAvatarContainer"}>
-                    <div className={"feedCardAvatar"}>
-                        <img src={"img/example-ava.jpg"} alt={"Avatar"}/>
+    const handleSubmit = async (values, { resetForm }) => {
+        try {
+            const postData = {
+                user: {
+                    userId: user.userId
+                },
+                textContent: values.textContent,
+                imageUrls: []
+            };
+
+            if (imagePost) {
+                const formData = new FormData();
+                formData.append("image", imagePost);
+
+                const response = await axios.post("http://localhost:8080/post-images", formData);
+                const imageUrl = response.data.imageUrl;
+                postData.imageUrls.push(imageUrl);
+            }
+
+            await axios.post("http://localhost:8080/posts", postData);
+            resetForm();
+        } catch (error) {
+            console.error("Error creating post:", error);
+        }
+    };
+
+    return (
+        <div className="newFeed">
+            <div className="newFeedContainer">
+                <br />
+
+                <div className="feedCarAvatarContainer">
+                    <div className="feedCardAvatar">
+                        <img src="img/example-ava.jpg" alt="Avatar" />
                     </div>
-                    <div className={"feedCardTextarea"}>
-                            <textarea style={{width: "80%"}} name="postContent"
-                                      placeholder={`${user.fullName} ơi, bạn đang nghĩ gì thế?`}></textarea>
-                        <button> Đăng</button>
+                    <div className="feedCardTextarea">
+                        <Formik
+                            initialValues={{
+                                textContent: "",
+                                authorizedView: ""
+                            }}
+                            onSubmit={handleSubmit}
+                        >
+                            <Form>
+                                <Field
+                                    name="textContent"
+                                    as="textarea"
+                                    placeholder={`${user.fullName} ơi, bạn đang nghĩ gì thế?`}
+                                    style={{ width: "80%" }}
+                                />
+                                <input
+                                    type="file"
+                                    name="file"
+                                    onChange={(event) => {
+                                        const file = event.currentTarget.files[0];
+                                        setImagePost(file);
+                                    }}
+                                />
+                                {/*<Field name="authorizedView" />*/}
+                                <button type="submit">Đăng</button>
+                            </Form>
+                        </Formik>
                     </div>
                 </div>
 

bc0e0ba7b327cc1b112c5a6cd9942a91ef482579
Tuong <aumanhtuong09122001@gmail.com>
2023-07-27 17:07:23 +0700

    them duoc nut dropdown ... cho moi bai viet va co the them cac <Menu.Item> neu can them cac const khac

--- src/pages/user/user_page/wall/Wall.js
+++ src/pages/user/user_page/wall/Wall.js
@@ -1,11 +1,14 @@
 import "./Wall.css";
 import {Link, useParams} from "react-router-dom";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import Swal from "sweetalert2";
 import {Field, Form, Formik} from "formik";
 import axios from "axios";
+import {Dropdown, Menu} from "antd";
 
 export default function Wall() {
+    // const [selectedOption, setSelectedOption] = useState('option1'); // Mặc định là "Mọi người"
+    const [dropDown, setDropDown] = useState(false)
     const [imagePost, setImagePost] = useState(null);
     const [userInformationWall, setUserInformationWall] = useState({})
     const {userId} = useParams();
@@ -28,7 +31,7 @@ export default function Wall() {
         return loggedInUser;
     })
 
-    const handleSubmit = async (values, { resetForm }) => {
+    const handleSubmit = async (values, {resetForm}) => {
         try {
             const postData = {
                 user: {
@@ -64,6 +67,9 @@ export default function Wall() {
         })
     }, [])
 
+    const handleShowDropDown = () => {
+        setDropDown(!dropDown); // Khi bấm vào thì đảo ngược trạng thái
+    };
 
     //Thông tin của 1 người
     useEffect(() => {
@@ -84,39 +90,40 @@ export default function Wall() {
     //Xoá 1 bài post của người đang đăng nhập
     const handleDeletePost = (postId) => {
         console.log("id của bài biết khi chuẩn bị xoá", postId)
-            Swal.fire({
-                icon: 'question',
-                title: 'Bạn có chắc muốn xoá không',
-                showCancelButton: true,
-                confirmButtonText: "Có",
-                cancelButtonText: "Không",
-                allowOutsideClick: true
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    axios.delete("http://localhost:8080/posts/" + postId).then((rp) => {
-                        console.log("trang thai cua Status", rp.status)
-                        if (rp.status === 200) {
-                            setPostListDisplay(postListDisplay.filter(post => post.postId !== postId));
-                            setPostList(postList.filter((s) => s.id !== postId));
-                            Swal.fire({
-                                icon: 'success',
-                                timer:1000
-                            })
-                        }
-                    })
-                }
+        Swal.fire({
+            icon: 'question',
+            title: 'Bạn có chắc muốn xoá không',
+            showCancelButton: true,
+            confirmButtonText: "Có",
+            cancelButtonText: "Không",
+            allowOutsideClick: true
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axios.delete("http://localhost:8080/posts/" + postId).then((rp) => {
+                    console.log("trang thai cua Status", rp.status)
+                    if (rp.status === 200) {
+                        setPostListDisplay(postListDisplay.filter(post => post.postId !== postId));
+                        setPostList(postList.filter((s) => s.id !== postId));
+                        Swal.fire({
+                            icon: 'success',
+                            timer: 1000
+                        })
+                    }
+                })
+            }
 
-            })
+        })
     }
+
 
     return (
         <div className="newFeed">
             <div className="newFeedContainer">
-                <br />
+                <br/>
 
                 <div className="feedCarAvatarContainer">
                     <div className="feedCardAvatar">
-                        <img src="img/example-ava.jpg" alt="Avatar" />
+                        <img src="img/example-ava.jpg" alt="Avatar"/>
                     </div>
                     <div className="feedCardTextarea">
                         <Formik
@@ -131,7 +138,7 @@ export default function Wall() {
                                     name="textContent"
                                     as="textarea"
                                     placeholder={`${user.fullName} ơi, bạn đang nghĩ gì thế?`}
-                                    style={{ width: "80%" }}
+                                    style={{width: "80%"}}
                                 />
                                 <input
                                     type="file"
@@ -160,17 +167,40 @@ export default function Wall() {
                                 <div className="feedCardHeaderInfo">
                                     <div>
                                         <div className="feedCardHeaderName">
-                                            <Link to={"/user/1"}><span> {userInformationWall.fullName} </span></Link>
+                                            <Link
+                                                to={`/user/${userInformationWall.userId}`}><span> {userInformationWall.fullName} </span></Link>
                                         </div>
-                                        {Number(user.userId) !== Number(userId) ? (
-                                            <div className="abcde">
-                                                <span> ••• </span>
-                                            </div>
-                                        ) : (
-                                            <div className="abcde">
-                                                <span onClick={() => handleDeletePost(item.postId)}> X </span>
-                                            </div>
-                                        )}
+                                        {
+                                            Number(user.userId) !== Number(userId) ? (
+                                                <Dropdown
+                                                    overlay={
+                                                        <Menu>
+                                                            <Menu.Item key="1">
+                                                                Ẩn bài viết
+                                                            </Menu.Item>
+                                                        </Menu>
+                                                    }
+                                                    trigger={["click"]}
+                                                >
+                                                    <span>•••</span>
+                                                </Dropdown>
+                                            ) : (
+                                                <Dropdown
+                                                    overlay={
+                                                        <Menu>
+                                                            <Menu.Item key="1"
+                                                                       onClick={() => handleDeletePost(item.postId)}>
+                                                                Xoá bài viết
+                                                            </Menu.Item>
+                                                        </Menu>
+                                                    }
+                                                    trigger={["click"]}
+                                                >
+                                                    <span>•••</span>
+                                                </Dropdown>
+                                            )
+
+                                        }
 
                                     </div>
 

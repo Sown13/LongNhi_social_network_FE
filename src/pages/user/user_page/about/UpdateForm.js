ed8a5dcb4a8803fd1f2ac73e6c3d6ccc7567b139
Tuong0192 <aumanhtuong2001@gmail.com>
2023-08-02 17:00:05 +0700

    dang thao tac voi UpdateForm, commit de tranh mat code :)

--- src/pages/user/user_page/about/UpdateForm.js
+++ src/pages/user/user_page/about/UpdateForm.js
@@ -8,71 +8,72 @@ import {ref, getDownloadURL, uploadBytesResumable} from "firebase/storage";
 import {storage} from "../../../../firebase";
 
 export default function UpdateForm() {
-    const { userId } = useParams();
+    const {userId} = useParams();
     const [user, setUser] = useState({});
     const [showAvatarModal, setShowAvatarModal] = useState(false);
     const [showBackgroundModal, setShowBackgroundModal] = useState(false);
 
-
+    const [avatarImage, setAvatarImage] = useState({});
+    const [backgroundImage, setBackgroundImage] = useState({});
 
     useEffect(() => {
         axios.get("http://localhost:8080/users/" + userId)
             .then((response) => {
                 setUser(response.data);
-                console.log("thong tin 1 user", response.data.background);
-                console.log("thong tin 1 user", showBackgroundModal);
-                console.log("ảnh nền của user", showAvatarModal);
+
+                console.log("avatar", avatarImage)
+                console.log("background", backgroundImage)
             })
             .catch((error) => {
                 console.error("Error fetching user:", error);
             });
-    }, [userId]); // Add userId as a dependency here
+    }, [userId]);
 
     const handleAvatarChange = (event) => {
         const file = event.currentTarget.files[0];
-        // Update the user state with the selected avatar file
-        setUser((prevUser) => ({
-            ...prevUser,
-            avatar: file
-        }));
+        setAvatarImage(file);
     };
 
     const handleBackgroundChange = (event) => {
         const file = event.currentTarget.files[0];
-        // Update the user state with the selected background file
-        setUser((prevUser) => ({
-            ...prevUser,
-            background: file
-        }));
+        setBackgroundImage(file);
     };
 
     const handleUpdateUser = async (values) => {
         try {
-            const avatarFile = user.avatar;
-            const backgroundFile = user.background;
+            let updatedAvatar = user.avatar;
+            let updatedBackground = user.background;
+            console.log("72", updatedAvatar)
+            console.log("73", updatedBackground)
 
-            const avatarStorageRef = ref(storage, `avatars/${user.avatar.name}`);
-            const backgroundStorageRef = ref(storage, `backgrounds/${user.background.name}`);
-
-            const avatarUploadTask = uploadBytesResumable(avatarStorageRef, avatarFile);
-            const backgroundUploadTask = uploadBytesResumable(backgroundStorageRef, backgroundFile);
-
-            await Promise.all([avatarUploadTask, backgroundUploadTask]);
-
-
-            const avatarDownloadURL = await getDownloadURL(avatarUploadTask.snapshot.ref);
-            const backgroundDownloadURL = await getDownloadURL(backgroundUploadTask.snapshot.ref);
-
+            if (avatarImage) {
+                const avatarStorageRef = ref(storage, `avatars/${user.avatar.name}`);
+                const avatarUploadTask = uploadBytesResumable(avatarStorageRef, avatarImage);
+                await avatarUploadTask;
+                const avatarDownloadURL = await getDownloadURL(avatarUploadTask.snapshot.ref);
+                updatedAvatar = avatarDownloadURL;
+                console.log("avatar - 80", avatarImage)
+            }
+            if (backgroundImage) {
+                const backgroundStorageRef = ref(storage, `backgrounds/${user.background.name}`);
+                const backgroundUploadTask = uploadBytesResumable(backgroundStorageRef, backgroundImage);
+                await backgroundUploadTask;
+                const backgroundDownloadURL = await getDownloadURL(backgroundUploadTask.snapshot.ref);
+                updatedBackground = backgroundDownloadURL;
+                console.log("background - 89", backgroundImage)
+            } else {
+                return;
+            }
 
             const updatedUser = {
                 ...values,
-                avatar: avatarDownloadURL,
-                background: backgroundDownloadURL,
+                avatar: updatedAvatar,
+                background: updatedBackground,
             };
 
-            // Send updated user data to the server
             await axios.put(`http://localhost:8080/users/${userId}`, updatedUser);
-            console.log("values up ảnh", values)
+            console.log("values up ảnh - 1", avatarImage);
+            console.log("values up ảnh background - 2", backgroundImage);
             setUser(updatedUser);
             await Swal.fire({
                 icon: "success",
@@ -85,6 +86,7 @@ export default function UpdateForm() {
         }
     };
 
+
     return (
         <>
             <div>
@@ -92,65 +94,121 @@ export default function UpdateForm() {
                     initialValues={user}
                     onSubmit={handleUpdateUser}
                     enableReinitialize={true}
+
                 >
                     <Form>
                         <table>
                             <tbody>
+                            <tr><td></td></tr>
+                            <tr>
+                                <td style={{textAlign: 'left'}}>
+                                    <strong style={{fontSize: '18px', fontWeight: 'bold'}}>Ảnh đại diện</strong>
+                                </td>
+                                <td style={{textAlign: 'right'}}>
+                                    <Dropdown
+                                        overlay={
+                                            <Menu>
+                                                <Menu.Item key="1">
+                                                    <td>Thay đổi ảnh đại diện</td>
+                                                    <input
+                                                        type="file"
+                                                        name="avatar"
+                                                        onChange={handleAvatarChange}
+                                                    />
+                                                </Menu.Item>
+                                                <Menu.Item key="2" onClick={() => setShowAvatarModal(true)}>
+                                                    Xem ảnh đại diện
+                                                </Menu.Item>
+                                            </Menu>
+                                        }
+                                        trigger={["click"]}
+                                    >
+                                        <strong style={{fontSize: '18px', fontWeight: 'bold'}}>Chỉnh sửa</strong>
+                                    </Dropdown>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                            </tr>
+                            <tr>
+
+                                <td colSpan={2} style={{ textAlign: 'left', padding: '10px', marginLeft: '200px' }}>
+                                    {
+                                        !user.avatar  ? (
+                                            <img
+                                                src="https://ss-images.saostar.vn/wp700/pc/1613810558698/Facebook-Avatar_3.png"
+                                                className="avatar"
+                                                style={{ textAlign: 'center', marginLeft: '130px', width: '190px', height: '190px' }}
+                                                alt="Default Avatar"
+                                            />
+                                        ) : (
+                                            <img
+                                                src={user.avatar}
+                                                className="avatar"
+                                                style={{ textAlign: 'center', marginLeft: '130px', width: '190px', height: '190px' }}
+                                                alt="User Avatar"
+                                            />
+                                        )
+                                    }
+                                </td>
+
+
+                            </tr>
+                            <tr>
+                                <td colSpan={2} style={{borderBottom: '1px solid #ccc'}}></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td style={{textAlign: 'left'}}>
+                                    <strong style={{fontSize: '18px', fontWeight: 'bold'}}>Ảnh bìa</strong>
+                                </td>
+                                <td style={{textAlign: 'right'}}>
+                                    <Dropdown
+                                        overlay={
+                                            <Menu>
+                                                <Menu.Item key="1">
+                                                    <td>Thay đổi ảnh bìa</td>
+                                                    <input
+                                                        type="file"
+                                                        name="avatar"
+                                                        onChange={handleBackgroundChange}
+                                                    />
+                                                </Menu.Item>
+                                                <Menu.Item key="2" onClick={() => setShowBackgroundModal(true)}>
+                                                    Xem ảnh đại diện
+                                                </Menu.Item>
+                                            </Menu>
+                                        }
+                                        trigger={["click"]}
+                                    >
+                                        <strong style={{fontSize: '18px', fontWeight: 'bold'}}>Chỉnh sửa</strong>
+                                    </Dropdown>
+                                    <tr><td></td></tr>
+                                </td>
+                            </tr>
                             <tr>
                                 <td colSpan={2}>
-                                    <div className="user-about-container">
-                                        <Dropdown
-                                            overlay={
-                                                <Menu className="custom-background-dropdown">
-                                                    <Menu.Item key="1">
-                                                        <td>Thay đổi ảnh nền</td>
-                                                        <input
-                                                            type="file"
-                                                            name="background"
-                                                            onChange={handleBackgroundChange}
-                                                        />
-                                                    </Menu.Item>
-                                                    <Menu.Item key="2" onClick={() => setShowBackgroundModal(true)}>
-                                                        Xem ảnh nền
-                                                    </Menu.Item>
-                                                </Menu>
-                                            }
-                                            trigger={['click']}
-                                        >
-                                            <img
-                                                src={user.background}
-                                                style={{fontSize: '20px', fontWeight: '50px'}}
-                                                className="cover-photo"
-                                            ></img>
-                                        </Dropdown>
-                                        <div className="user-profile">
-                                            <div className={"delete-post-button"}>
-                                                <Dropdown
-                                                    overlay={
-                                                        <Menu>
-                                                            <Menu.Item key="1">
-                                                                <td>Thay đổi ảnh đại diện</td>
-                                                                <input
-                                                                    type="file"
-                                                                    name="avatar"
-                                                                    onChange={handleAvatarChange}
-                                                                />
-                                                            </Menu.Item>
-                                                            <Menu.Item key="2" onClick={() => setShowAvatarModal(true)}>
-                                                                Xem ảnh đại diện
-                                                            </Menu.Item>
-                                                        </Menu>
-                                                    }
-                                                    trigger={["click"]}
-                                                >
-                                                    <img src={user.avatar} className="avatar" alt="User Avatar"/>
-                                                </Dropdown>
-                                            </div>
-                                        </div>
-                                    </div>
+                                        <img
+                                            src={user.background}
+                                            style={{fontSize: '20px', fontWeight: '50px'}}
+                                            className="cover-photo"
+                                        ></img>
                                 </td>
 
                             </tr>
+
+                            <tr>
+                                <td colSpan={2} style={{borderBottom: '1px solid #ccc'}}></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                            </tr>
+
+                            <strong style={{fontSize: '18px', fontWeight: 'bold'}}>Thông tin cá nhân</strong>
+                            <tr><td></td></tr>
+
                             <tr>
                                 <td><strong>Tên tài khoản</strong></td>
                                 <Field type="text" name="accountName"/>
@@ -180,10 +238,8 @@ export default function UpdateForm() {
                                 <Field type="text" name={"address"} v></Field>
                             </tr>
                             <tr>
-                            </tr>
-                            <tr>
                                 <td colSpan="2">
-                                    <button type="submit" style={{ backgroundColor: '#ff6347' }}>Lưu</button>
+                                    <button type="submit" style={{backgroundColor: '#ff6347'}}>Lưu</button>
                                 </td>
                             </tr>
                             </tbody>
@@ -191,22 +247,23 @@ export default function UpdateForm() {
                     </Form>
                 </Formik>
             </div>
-            <Modal
 
+            <Modal
                 visible={showAvatarModal}
                 onCancel={() => setShowAvatarModal(false)}
                 footer={null}
             >
-                <img src={user.avatar} alt="User Avatar" style={{width:'470px', height:'500px', marginTop:'20px'}}/>
+                <img src={user.avatar} alt="User Avatar" style={{width: '470px', height: '500px', marginTop: '20px'}}/>
             </Modal>
             <Modal
 
-            visible={showBackgroundModal}
-            onCancel={() => setShowBackgroundModal(false)}
-            footer={null}
-        >
-            <img src={user.background} alt="User Avatar" style={{width: '470px', height: '500px', marginTop: '20px'}}/>
-        </Modal>
+                visible={showBackgroundModal}
+                onCancel={() => setShowBackgroundModal(false)}
+                footer={null}
+            >
+                <img src={user.background} alt="User Avatar"
+                     style={{width: '470px', height: '500px', marginTop: '20px'}}/>
+            </Modal>
         </>
     );
 };

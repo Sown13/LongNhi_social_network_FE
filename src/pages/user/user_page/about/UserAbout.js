e71777b19c33a4c6e4d3b6585164357a0a5f1ba3
Tuong0192 <aumanhtuong2001@gmail.com>
2023-08-02 10:34:20 +0700

    check pull co conflict hay khong

--- src/pages/user/user_page/about/UserAbout.js
+++ src/pages/user/user_page/about/UserAbout.js
@@ -73,37 +73,37 @@ export default function UserAbout() {
 
                         {
                             Number(user.userId) !== Number(userId) ? (
-                                    <MDBCard className="mb-4 mb-lg-0">
+                                <MDBCard className="mb-4 mb-lg-0">
                                     <MDBCardBody className="p-0">
-                                <MDBListGroup flush className="rounded-3">
-                                    <MDBListGroupItem
-                                        className="d-flex justify-content-between align-items-center p-3">
-                                        <MDBIcon fas icon="globe fa-lg text-warning"/>
-                                        <MDBCardText>https://mdbootstrap.com</MDBCardText>
-                                    </MDBListGroupItem>
-                                    <MDBListGroupItem
-                                        className="d-flex justify-content-between align-items-center p-3">
-                                        <MDBIcon fab icon="github fa-lg" style={{color: '#333333'}}/>
-                                        <MDBCardText>mdbootstrap</MDBCardText>
-                                    </MDBListGroupItem>
-                                    <MDBListGroupItem
-                                        className="d-flex justify-content-between align-items-center p-3">
-                                        <MDBIcon fab icon="twitter fa-lg" style={{color: '#55acee'}}/>
-                                        <MDBCardText>@mdbootstrap</MDBCardText>
-                                    </MDBListGroupItem>
-                                    <MDBListGroupItem
-                                        className="d-flex justify-content-between align-items-center p-3">
-                                        <MDBIcon fab icon="instagram fa-lg" style={{color: '#ac2bac'}}/>
-                                        <MDBCardText>mdbootstrap</MDBCardText>
-                                    </MDBListGroupItem>
-                                    <MDBListGroupItem
-                                        className="d-flex justify-content-between align-items-center p-3">
-                                        <MDBIcon fab icon="facebook fa-lg" style={{color: '#3b5998'}}/>
-                                        <MDBCardText>mdbootstrap</MDBCardText>
-                                    </MDBListGroupItem>
-                                </MDBListGroup>
-                            </MDBCardBody>
-                        </MDBCard>
+                                        <MDBListGroup flush className="rounded-3">
+                                            <MDBListGroupItem
+                                                className="d-flex justify-content-between align-items-center p-3">
+                                                <MDBIcon fas icon="globe fa-lg text-warning"/>
+                                                <MDBCardText>https://mdbootstrap.com</MDBCardText>
+                                            </MDBListGroupItem>
+                                            <MDBListGroupItem
+                                                className="d-flex justify-content-between align-items-center p-3">
+                                                <MDBIcon fab icon="github fa-lg" style={{color: '#333333'}}/>
+                                                <MDBCardText>mdbootstrap</MDBCardText>
+                                            </MDBListGroupItem>
+                                            <MDBListGroupItem
+                                                className="d-flex justify-content-between align-items-center p-3">
+                                                <MDBIcon fab icon="twitter fa-lg" style={{color: '#55acee'}}/>
+                                                <MDBCardText>@mdbootstrap</MDBCardText>
+                                            </MDBListGroupItem>
+                                            <MDBListGroupItem
+                                                className="d-flex justify-content-between align-items-center p-3">
+                                                <MDBIcon fab icon="instagram fa-lg" style={{color: '#ac2bac'}}/>
+                                                <MDBCardText>mdbootstrap</MDBCardText>
+                                            </MDBListGroupItem>
+                                            <MDBListGroupItem
+                                                className="d-flex justify-content-between align-items-center p-3">
+                                                <MDBIcon fab icon="facebook fa-lg" style={{color: '#3b5998'}}/>
+                                                <MDBCardText>mdbootstrap</MDBCardText>
+                                            </MDBListGroupItem>
+                                        </MDBListGroup>
+                                    </MDBCardBody>
+                                </MDBCard>
 
                             ) : (
                                 <MDBCard>

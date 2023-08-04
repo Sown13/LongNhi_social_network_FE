9bbb901ac7d124eb1266d00752140b8097cb03bf
giangthidua <125744426+giangadua06042001@users.noreply.github.com>
2023-08-04 10:09:04 +0700

    ready to pull DUng

--- src/pages/user/user_page/photo/UserPhoto.js
+++ src/pages/user/user_page/photo/UserPhoto.js
@@ -1,9 +1,34 @@
-export default function UserPhoto(){
+import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import "./UserPhoto.css"
+
+export default function UserPhoto() {
+    const {userId} = useParams();
+    const [listImage, setListImage] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get(`http://localhost:8080/post-images/user/${userId}`)
+            .then(res => {
+                setListImage(res.data);
+                console.log(JSON.stringify(res.data));
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }, [userId]);
+
     return (
-        <div>
-            <h1>
-                user photo
-            </h1>
-        </div>
-    )
+
+
+                <div className="image">
+                    {listImage.map(image => (
+                        <div className="image-album">
+                            <img key={image.postImageId} src={image.imgUrl} alt="user image"/>
+                        </div>
+
+                    ))}
+                </div>
+    );
 }

bd3206402af03205742fe4018ecf04fd3efa2bfe
Tuong0192 <aumanhtuong2001@gmail.com>
2023-08-07 11:47:09 +0700

    onclick open picture, next picture if have more than 1 and paddingBottom of the picture

--- src/components/image/ImageList.js
+++ src/components/image/ImageList.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { Modal } from "antd";
+import "./Images.css"
 
 const ImageList = ({ images, imagesPerRow }) => {
     const [containerWidth, setContainerWidth] = useState(0);
     const imageWidth = containerWidth / imagesPerRow;
 
-    const [showPictureModalIndex, setShowPictureModalIndex] = useState(-1); // -1 means no modal is shown
+    const [showPictureModal, setShowPictureModal] = useState(false);
+    const [currentImageIndex, setCurrentImageIndex] = useState(0); // Index of the currently displayed image
 
     useEffect(() => {
         const handleResize = () => {
@@ -21,6 +23,23 @@ const ImageList = ({ images, imagesPerRow }) => {
         };
     }, []);
 
+    const openPictureModal = (index) => {
+        setCurrentImageIndex(index);
+        setShowPictureModal(true);
+    };
+
+    const nextImage = () => {
+        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    };
+
+    const prevImage = () => {
+        setCurrentImageIndex((prevIndex) =>
+            prevIndex === 0 ? images.length - 1 : prevIndex - 1
+        );
+    };
+
+    const shouldShowButtons = images.length > 1;
+
     return (
         <div
             id="image-list"
@@ -32,22 +51,75 @@ const ImageList = ({ images, imagesPerRow }) => {
             }}
         >
             {images.map((image, index) => (
-                <div key={index} style={{ width: `${imageWidth}px`, margin: "1px" }}>
+                <div key={index} style={{ width: `${imageWidth}px`, marginTop: "1px" }}>
                     <img
-                        style={{ maxWidth: "100%", height: "auto", aspectRatio: `${image.width} / ${image.height}` }}
+                        style={{
+                            maxWidth: "100%",
+                            height: "auto",
+                            aspectRatio: `${image.width} / ${image.height}`,
+                        }}
                         src={image.imgUrl}
                         alt={`ảnh ${index}`}
-                        onClick={() => setShowPictureModalIndex(index)} // Store the index of the clicked image
+                        onClick={() => openPictureModal(index)} // Store the index of the clicked image
                     />
-                    <Modal
-                        visible={showPictureModalIndex === index} // Only show the modal if the index matches
-                        onCancel={() => setShowPictureModalIndex(-1)} // Hide the modal by resetting the index
-                        footer={null}
-                    >
-                        <img src={image.imgUrl} alt="User Avatar" style={{ width: '470px', height: '500px', marginTop: '20px' }} />
-                    </Modal>
                 </div>
             ))}
+            <Modal
+                visible={showPictureModal}
+                onCancel={() => setShowPictureModal(false)}
+                footer={null}
+                centered // Để căn giữa modal
+
+            >
+                <div style={{ position: "relative", paddingBottom: "20px" }} className={"ant-modal-body"}>
+                    {shouldShowButtons && (
+                        <>
+                            <button
+                                onClick={() => prevImage()}
+                                style={{
+                                    position: "absolute",
+                                    top: "50%",
+                                    left: "8px",
+                                    background: "transparent",
+                                    border: "none",
+                                    color: "silver",
+                                    fontSize: "24px",
+                                    cursor: "pointer",
+                                    transform: "translateY(-50%)",
+                                }}
+                            >
+                                <i className="fas fa-chevron-left"></i>
+                            </button>
+                            <button
+                                onClick={() => nextImage()}
+                                style={{
+                                    position: "absolute",
+                                    top: "50%",
+                                    right: "8px",
+                                    background: "transparent",
+                                    border: "none",
+                                    color: "silver",
+                                    fontSize: "24px",
+                                    cursor: "pointer",
+                                    transform: "translateY(-50%)",
+                                }}
+                            >
+                                <i className="fas fa-chevron-right"></i>
+                            </button>
+                        </>
+                    )}
+                    <img
+                        src={images[currentImageIndex].imgUrl}
+                        alt={`ảnh ${currentImageIndex}`}
+                        style={{
+                            width: "470px",
+                            height: "500px",
+                            marginTop: "20px",
+                            objectFit: "contain",
+                        }}
+                    />
+                </div>
+            </Modal>
         </div>
     );
 };
